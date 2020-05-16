93eaa104d2d1589a7acd99c83038751832140eea
sanjeet123456789 <sanjeetpal123456789@gmail.com>
2020-05-16 20:25:34 +0530

    spring initilizer with travis yml file

---
+++ src/main/java/com/sanjeet/pal/test/gettingbuildtags/GettingBuildTagsApplication.java
@@ -0,0 +1,13 @@
+package com.sanjeet.pal.test.gettingbuildtags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingBuildTagsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingBuildTagsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanjeet/pal/test/gettingbuildtags/GettingBuildTagsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjeet.pal.test.gettingbuildtags;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingBuildTagsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
